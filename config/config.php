7e132785a283782bb0ad781ac57d415d342a481c
zeta <jersson.z032@gmail.com>
2024-05-09 16:59:01 -0500

    correcciones

--- config/config.php
+++ config/config.php
@@ -4,9 +4,9 @@ define('URL', 'http://localhost/planilla/');
 
 define('COMPANY', 'Planillas - DRTC');
 define('HOST', 'localhost');
-define('USER', 'usuario');
+define('USER', 'jersson');
 define('DB', 'mtc');
-define('PASSWORD', 'archivo123$');
+define('PASSWORD', 'jersson');
 define('CHATSET', 'utf8');
 
 date_default_timezone_set('America/Lima');
